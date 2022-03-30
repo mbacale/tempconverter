ec329fa9d004cdbc4d6e16caefd5617f85a8ffb8
Baca <mbaca@FVFZMYBAJ1WK.tld>
2022-03-30 14:11:01 -0500

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+function askName() {
+    var name = prompt("what is your name")
+    return name 
+} 
+
+function genName() {
+    var number = Math.floor(Math.random() * 10 +1);
+
+}
+
+
+
+genName()
